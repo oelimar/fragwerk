bfb89244988e1197a7c69b12c60680d04ddd2599
oelimar <154346803+oelimar@users.noreply.github.com>
2024-01-17 15:25:16 +0100

    Update fragwerk_test.py

--- fragwerk_test.py
+++ fragwerk_test.py
@@ -15,7 +15,7 @@ import requests # um material.json auf github abzufragen
 st.set_page_config(
     page_title="fragwerk Fachwerkrechner",
     layout="wide",
-    page_icon=":abacus:"
+    page_icon="https://raw.githubusercontent.com/oelimar/images/main/fragwerk.png"
 )
 
 #svg_path = r"C:\Users\DerSergeant\Desktop\fragwerk\fragwerk.png"
