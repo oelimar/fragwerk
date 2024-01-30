4b3453f5620a5e798dcff65d961dd55f75c58b3d
oelimar <154346803+oelimar@users.noreply.github.com>
2024-01-30 20:10:34 +0100

    Add files via upload

--- fragwerk_test.py
+++ fragwerk_test.py
@@ -298,9 +298,12 @@ with st.container(border=True):
             roofType = st.selectbox("Dachaufbau", placeholder="Wähle einen Dachaufbau", index=1, options=roofOptions, label_visibility="collapsed")
             with check:
                 correct_input(roofType)
-                if st.session_state["reload"] == True:
-                    st.rerun
-                    st.session_state["reload"] = False
+                try:
+                    if st.session_state["reload"] == True:
+                        st.rerun
+                        st.session_state["reload"] = False
+                except:
+                    st.empty()
             #Anzeige des Diagramms in Abhängigkeit zu Auswahl
             st.image(roofImage[roofType], use_column_width=True)
 
